5c4479df58db8148e1f3ce57b97aeaf4071af6a5
rafliiar17 <rafztesting@gmail.com>
2026-09-03 07:33:37 +0700

    fix(geoglobe): remove stale worldCapitalsApp import in geoStore

--- frontend/src/lib/framework/geoglobe/geoStore.svelte.ts
+++ frontend/src/lib/framework/geoglobe/geoStore.svelte.ts
@@ -6,7 +6,6 @@ import { worldTimeApp } from './plugins/worldTimeApp';
 import { flowCorridorsApp } from './plugins/flowCorridorsApp';
 import { passportWorldApp } from './plugins/passportWorldApp';
 import { floraFaunaApp } from './plugins/floraFaunaApp';
-import { worldCapitalsApp } from './plugins/worldCapitalsApp';
 import { resolvePathToAppId, resolveAppIdToPath } from './router';
 import {
   type TimeFilterType,
@@ -32,7 +31,6 @@ geoRegistry.register(worldTimeApp);
 geoRegistry.register(flowCorridorsApp);
 geoRegistry.register(passportWorldApp);
 geoRegistry.register(floraFaunaApp);
-geoRegistry.register(worldCapitalsApp);
 
 export function createGeoStore() {
   const initialAppId = typeof window !== 'undefined'
